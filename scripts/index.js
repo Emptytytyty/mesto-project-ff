97947988a15e2e6a87a1f6a60fe50eb8d14fe16c
Empty <empty12413@yandex.ru>
2025-04-08 22:06:10 +0900

    feat: added logic for creating and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(cardName, cardImageLink, deleteCallback) {
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+
+  newCard.querySelector('.card__title').textContent = cardName;
+  newCard.querySelector('.card__image').src = cardImageLink;
+
+  const cardDeleteButton = newCard.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', deleteCallback);
+
+  return newCard;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard () {
+  this.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+  const cardToAdd = createCard(item.name, item.link, deleteCard);
+  document.querySelector('.places__list').append(cardToAdd);
+})
